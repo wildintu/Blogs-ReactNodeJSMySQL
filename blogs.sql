6105e9adcea1d532f437804659035f7908c525d0
wildintu <hello@wildintu.com>
2020-03-30 17:20:55 -0500

    added blogs sql database file

--- blogs.sql
+++ blogs.sql
@@ -1,64 +1,129 @@
-select * from tags;
-use blogs;
-select * from blogtags;
-select * from tags;
-select * from blogs;
+-- MySQL dump 10.13  Distrib 5.7.29, for macos10.14 (x86_64)
+--
+-- Host: 127.0.0.1    Database: blogs
+-- ------------------------------------------------------
+-- Server version	5.7.29
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `authors`
+--
 
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-insert into blogs (title, content, authorid)
-values('what in the world', 'hello world', 3);
+--
+-- Dumping data for table `authors`
+--
 
-insert into tags (name)
-values('TRex');
-insert into tags (name)
-values('Burn');
-insert into tags (name)
-values('PepeHands');
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+INSERT INTO `authors` VALUES (1,'Kim','abc@123.com','2020-03-23 13:08:44'),(2,'Linda','xyz@123.com','2020-03-23 13:08:44'),(3,'Yoda','saber@123.com','2020-03-23 13:08:44');
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
 
-insert into blogtags (blogid,tagid)
-values(8,4);
-insert into blogtags (tagid)
-values(3);
-insert into blogtags (tagid)
-values(1);
-insert into blogtags (tagid)
-values(5);
-insert into blogtags (tagid)
-values(6);
-insert into blogtags (tagid)
-values(8);
+--
+-- Table structure for table `blogs`
+--
 
-insert into authors (name, email)
-values('Kim', 'abc@123.com');
-insert into authors (name, email)
-values('Linda', 'xyz@123.com');
-insert into authors (name, email)
-values('Yoda', 'saber@123.com');
+DROP TABLE IF EXISTS `blogs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blogs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(100) NOT NULL,
+  `authorid` int(11) NOT NULL,
+  `_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-select * from blogs;
-select * from authors;
+--
+-- Dumping data for table `blogs`
+--
 
-insert into blogs (title, content, authorid)
-values('How to do this', 'i cannot teach you bc i do not know how to teach you', '2');
-insert into blogs (title, content, authorid)
-values('This is so hard', 'how am i going to be able to actuall apply this', '1');
-insert into blogs (title, content, authorid)
-values('Send halp', 'i am on a major struggle bus right now', '3');
-insert into blogs (title, content, authorid)
-values('Run away', 'i am running away to a cave', '2');
+LOCK TABLES `blogs` WRITE;
+/*!40000 ALTER TABLE `blogs` DISABLE KEYS */;
+INSERT INTO `blogs` VALUES (4,'Taffy','Woof',3,'2020-03-24 08:49:14'),(6,'hello world','yeah yeah yeah yeah',3,'2020-03-25 16:14:35'),(7,'what in the world','hello world',3,'2020-03-25 16:37:30'),(11,'but then i told him','sdfjsfjsajfk',2,'2020-03-25 16:54:43'),(12,'queen ','yup',3,'2020-03-25 20:13:22'),(14,'Whatever you must','This is how we do it',3,'2020-03-30 13:20:43'),(15,'one more time','kdjfajsfijweifajowe',3,'2020-03-30 14:43:56'),(16,'dddddd','ddddddd',1,'2020-03-30 14:57:45'),(17,'new blog','testing',2,'2020-03-30 14:58:22'),(18,'okie dokie','one lsat ime',2,'2020-03-30 15:10:35');
+/*!40000 ALTER TABLE `blogs` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `blogtags`
+--
 
-insert into users (name, email, password)
-values('Lisa', 'abc@123.com', 'unicorn');
-insert into users (name, email, password)
-values('Larry', 'abc@123.com', 'unicorn');
-insert into users (name, email, password)
-values('Susan', 'abc@123.com', 'unicorn');
-insert into users (name, email, password)
-values('Amy', 'abc@123.com', 'unicorn');
-insert into users (name, email, password)
-values('Jon', 'abc@123.com', 'unicorn');
-insert into users (name, email, password)
-values('Bob', 'abc@123.com', 'unicorn');
+DROP TABLE IF EXISTS `blogtags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blogtags` (
+  `blogid` int(11) NOT NULL,
+  `tagid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`blogid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blogtags`
+--
+
+LOCK TABLES `blogtags` WRITE;
+/*!40000 ALTER TABLE `blogtags` DISABLE KEYS */;
+INSERT INTO `blogtags` VALUES (1,3),(2,1),(3,1),(4,2),(8,4),(9,3),(10,1),(13,2),(14,2),(15,10),(16,9),(17,5),(18,6);
+/*!40000 ALTER TABLE `blogtags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (1,'Coding','2020-03-23 11:07:34'),(2,'Other','2020-03-23 11:07:55'),(3,'Testing','2020-03-23 11:12:13'),(4,'Whale','2020-03-23 17:46:30'),(5,'Cat','2020-03-23 17:46:30'),(6,'Coronavirus','2020-03-23 17:46:30'),(7,'Jupiter','2020-03-23 20:54:06'),(8,'TRex','2020-03-23 20:54:06'),(9,'Burn','2020-03-23 20:54:06'),(10,'PepeHands','2020-03-23 20:54:06');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-30 17:11:09
